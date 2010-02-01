2326e109d85fa3f3ae8934f24059594e3da76512
hiboma <hiroyan@gmail.com>
2010-02-01 21:25:05 +0900

    make tidy + 業務的な仕様をサポートするコード追加(...スルーしてね)

--- mod_dynamic_include.c
+++ mod_dynamic_include.c
@@ -19,32 +19,42 @@ static bool is_secure_path(const char *path) {
     return true;
 }
 
-MODRET set_dynamic_include_path(cmd_rec *cmd) {
+MODRET set_dynamic_include(cmd_rec *cmd) {
   CHECK_ARGS(cmd, 1);
   CHECK_CONF(cmd, CONF_ROOT|CONF_GLOBAL);
 
   /* 全プロセスで共有していいのでstaticな領域に確保 */
-  path_format = pstrdup(main_server->pool, cmd->argv[1]);
+  path_format = pstrdup(cmd->server->pool, cmd->argv[1]);
   pr_log_debug(DEBUG3,
-               "%s: set DynamicIncludePath '%s'", MODULE_NAME, path_format);
+    "%s: set DynamicIncludePath '%s'", MODULE_NAME, path_format);
 
   return PR_HANDLED(cmd);
 }
 
 MODRET dynamic_include_post_pass(cmd_rec *cmd) {
   const char *conf = NULL;
-  const char *user = NULL;
+  char *user = NULL;
 
-  /* todo */
   user = get_param_ptr(cmd->server->conf, C_USER, FALSE);
-  conf = sreplace(cmd->tmp_pool, path_format, "%u", user, NULL);
+
+#ifdef _SUPPORT_SUBACCOUNT
+  char *found;
+  user = pstrdup(cmd->tmp_pool, user);
+  if((found = strchr(user, '_'))) {
+      user[found - user] = '\0';
+  }
+#endif
+
+  /* todo ( if you want another formats string )*/
+  conf = sreplace(cmd->tmp_pool, path_format,
+              "%u", pstrdup(cmd->tmp_pool, user), NULL);
 
   pr_log_debug(DEBUG3,
-               "%s: try to Include '%s'", MODULE_NAME, conf);
+    "%s: try to Include '%s'", MODULE_NAME, conf);
 
   if(!is_secure_path(conf)) {
     pr_log_pri(PR_LOG_WARNING,
-               "%s: config path is insecure '%s'", MODULE_NAME, conf);
+      "%s: config path is insecure '%s'", MODULE_NAME, conf);
     pr_response_send(R_530, _("Login Denied."));
     end_login(0);
   }
@@ -52,15 +62,15 @@ MODRET dynamic_include_post_pass(cmd_rec *cmd) {
   /* パスが不正なら読み込みしないでスルーする */
   if (pr_fs_valid_path(conf) < 0) {
     pr_log_pri(PR_LOG_NOTICE,
-               "%s: unable to use path for configuration file '%s'", MODULE_NAME, conf);
+      "%s: unable to use path for configuration file '%s'", MODULE_NAME, conf);
     return PR_DECLINED(cmd);
   }
 
   /* pr_parser_prepare()呼び出しておかないとSIGSEGV出す */
-  pr_parser_prepare(main_server->pool, NULL);
-  if(pr_parser_parse_file(main_server->pool, conf, NULL, 0) == 0 ) {
+  pr_parser_prepare(cmd->server->pool, NULL);
+  if(pr_parser_parse_file(cmd->server->pool, conf, NULL, 0) == 0 ) {
       pr_log_debug(DEBUG3,
-                   "%s: OK parse_config_path('%s')", MODULE_NAME, conf);
+        "%s: OK parse_config_path('%s')", MODULE_NAME, conf);
   } else {
     if (errno != EINVAL)
       pr_log_pri(PR_LOG_WARNING, "warning: unable to include '%s': %s",
@@ -75,7 +85,7 @@ MODRET dynamic_include_post_pass(cmd_rec *cmd) {
 }
 
 static conftable dynamic_include_conftab[] = {
-  { "DynamicIncludePath", set_dynamic_include_path, NULL },
+  { "DynamicInclude", set_dynamic_include, NULL },
   { NULL },
 };
 
